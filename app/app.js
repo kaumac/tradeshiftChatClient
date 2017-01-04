a029b3e9c448514d29bbd62e571129338462aa73
Kaue Miyauti Machado <kaumac@gmail.com>
2017-01-04 12:30:17 +0100

    render react app on child element on body tag

--- app/app.js
+++ app/app.js
@@ -1,7 +1,25 @@
 import React from "react";
-import Message from "./components/message/message";
+import ReactDOM from "react-dom";
+import MessageList from "./containers/MessageList";
 
-React.render(
-  <Message name="Kaue"/>,
-  document.body
-);
+const appRootElement = document.getElementById('chat-app');
+
+let messagesMock = [
+ { 
+    author: "Kaue",
+    date: "random date",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, voluptates."
+  },
+  { 
+    author: "Someone",
+    date: "random date c",
+    text: "Lorem ipsum dolor sit amet."
+  },
+  { 
+    author: "Random dude",
+    date: "random date 2",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloribus ipsam, quibusdam consequatur accusamus deleniti!"
+  }
+]
+
+ReactDOM.render(<MessageList messages= { messagesMock } />, appRootElement);

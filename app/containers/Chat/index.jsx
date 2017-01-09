f595395d2b80ba032254d77e7c3a10b779475096
Kaue Miyauti Machado <kaumac@gmail.com>
2017-01-09 11:23:50 +0100

    refactor Chat component to export function instead of class

--- app/containers/Chat/index.jsx
+++ app/containers/Chat/index.jsx
@@ -3,35 +3,35 @@ import MessageList from 'containers/MessageList';
 import ChatForm from 'components/ChatForm';
 require('./style.scss');
 
-const messagesMock = [
-  {
-    author: 'Kaue',
-    date: 'random date',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, voluptates.',
-  },
-  {
-    author: 'Someone',
-    date: 'random date c',
-    text: 'Lorem ipsum dolor sit amet.',
-  },
-  {
-    author: 'Random dude',
-    date: 'random date 2',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos consequatur accusamus deleniti!',
-  },
-];
+const Chat = () => {
+  const messagesMock = [
+    {
+      author: 'Kaue',
+      date: 'random date',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, voluptates.',
+    },
+    {
+      author: 'Someone',
+      date: 'random date c',
+      text: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+      author: 'Random dude',
+      date: 'random date 2',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos consequatur accusamus deleniti!',
+    },
+  ];
 
-export default class Chat extends React.Component {
-  submitNewMessage(messageAuthor, messageText) {
+  function submitNewMessage(messageAuthor, messageText) {
     console.log(messageAuthor, messageText);
   }
 
-  render() {
-    return(
-      <div className='chat-wrapper'>
-        <MessageList messages={messagesMock} />
-        <ChatForm submitHandler={this.submitNewMessage.bind(this)} />
-      </div>
-    );
-  }
+  return(
+    <div className='chat-wrapper'>
+      <MessageList messages={messagesMock} />
+      <ChatForm submitHandler={submitNewMessage.bind(this)} />
+    </div>
+  )
 }
+
+export default Chat;

40c672d6593823222456e02c67602cc7c4136bbe
Kaue Miyauti Machado <kaumac@gmail.com>
2017-01-09 13:26:05 +0100

    refactor Message component to export function

--- app/components/Message/index.jsx
+++ app/components/Message/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 require('./style.scss');
 
-export default class Message extends React.Component {  
-  render() {
-    return (<div className="message"> { this.props.message.text } </div>)
-  }
-}
+const Message = ({ author, text }) => (
+  <div className="message">
+    <div>
+      { author }>
+    </div>
+    <div>
+      { text }>
+    </div>
+  </div>
+)
 
 Message.propTypes = {
-  message: React.PropTypes.array.isRequired
+  author: React.PropTypes.string.isRequired,
+  text: React.PropTypes.string.isRequired
 }
+
+export default Message;
